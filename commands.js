3a700be441b2692181f529312485675d0cda1283
Mahesh Kumar Thiagarajan <maheshkumarthiagarajan@Maheshs-MacBook-Pro.local>
2020-06-24 12:15:37 -0500

    removed comments

--- commands.js
+++ commands.js
@@ -27,11 +27,8 @@ Cypress.Commands.add('isNotInViewport', element => {
   cy.get(element).then($el => {
     const bottom = Cypress.$(cy.state('window')).height()
     const rect = $el[0].getBoundingClientRect()
-
     expect(rect.top).to.be.greaterThan(bottom)
-    //expect(rect.bottom).to.be.greaterThan(bottom)
     expect(rect.top).to.be.greaterThan(bottom)
-    //expect(rect.bottom).to.be.greaterThan(bottom)
   })
 })
 
@@ -39,10 +36,7 @@ Cypress.Commands.add('isInViewport', element => {
   cy.get(element).then($el => {
     const bottom = Cypress.$(cy.state('window')).height()
     const rect = $el[0].getBoundingClientRect()
-
     expect(rect.top).not.to.be.greaterThan(bottom)
-    //expect(rect.bottom).not.to.be.greaterThan(bottom)
     expect(rect.top).not.to.be.greaterThan(bottom)
-    //expect(rect.bottom).not.to.be.greaterThan(bottom)
   })
 })
